f5035e1188f2cdb8c3f686b402e80c8c7cb08bdd
fsluizvictor <fs.luizvictor@gmail.com>
2021-01-28 00:30:04 -0300

    add connection with database

--- server/src/database/models/User.ts
+++ server/src/database/models/User.ts
@@ -1,8 +1,12 @@
 export type TUser = {
     id: number
     name: string
+    image: string
     registration: string
     city: string
+    uf: string
+    latitude: number
+    longitude: number
     birth: string
     course: string
     email: string
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,9 @@
 import express from 'express'
+import routes from './routes'
 
 const app = express()
 
 app.use(express.json())
-
-app.get('/users', (request, response) => {
-    console.log('teste')
-})
-
+app.use(routes)
 app.listen(3333)
 
- 
