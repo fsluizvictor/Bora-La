8a42aecf01a69da98d3e89dfc8d0d86379e6085b
fsluizvictor <fs.luizvictor@gmail.com>
2021-02-11 01:26:40 -0300

    add json with all braziliam colleges

--- web/src/pages/CreateUser/index.tsx
+++ web/src/pages/CreateUser/index.tsx
@@ -1,9 +1,119 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { FiArrowLeft } from 'react-icons/fi'
+
+import logo from '../../assets/logo/logo.svg'
+
+import './styles.css'
 
 const CreateUser = () => {
     return (
-        <div>
-            
+        <div id="page-create-point">
+            <header>
+                <img src={logo} alt="Bora Lá" />
+
+                <Link to="/">
+                    <FiArrowLeft />
+                Voltar para home
+                </Link>
+            </header>
+            <form>
+                <h1>Cadastro do Estudante :)</h1>
+
+                <fieldset>
+                    <legend>
+                        <h2>Dados</h2>
+                    </legend>
+                    <div className="field">
+                        <label htmlFor="name">Nome Completo</label>
+                        <input
+                            type="text"
+                            name="name"
+                            id="name"
+                        />
+                    </div>
+
+                    <div className="field-group">
+                        <div className="field">
+                            <label htmlFor="name">E-mail</label>
+                            <input
+                                type="email"
+                                name="email"
+                                id="email"
+                            />
+                        </div>
+                        <div className="field">
+                            <label htmlFor="whatsapp">Whatsapp</label>
+                            <input
+                                type="text"
+                                name="whatsapp"
+                                id="whatsapp"
+                            />
+                        </div>
+                    </div>
+                    <div className="field-group">
+                        <div className="field">
+                            <label htmlFor="name">Matrícula</label>
+                            <input
+                                type="email"
+                                name="email"
+                                id="email"
+                            />
+                        </div>
+                        <div className="field">
+                            <label htmlFor="whatsapp">Data de Nascimento</label>
+                            <input
+                                type="text"
+                                name="whatsapp"
+                                id="whatsapp"
+                            />
+                        </div>
+                    </div>
+                    <div className="field">
+                        <label htmlFor="name">Curso</label>
+                        <select name="uf" id="uf">
+                            <option value="">Selecione um Curso</option>
+                        </select>
+                    </div>
+                    <div className="field">
+                        <label htmlFor="name">Senha</label>
+                        <input
+                            type="text"
+                            name="name"
+                            id="name"
+                        />
+                    </div>
+                </fieldset>
+
+                <fieldset>
+                    <legend>
+                        <h2>Endereço</h2>
+                        <span>Selecione o endereço no mapa</span>
+                    </legend>
+                    <div className="field-group">
+                        <div className="field">
+                            <label htmlFor="">Estado (UF)</label>
+                            <select name="uf" id="uf">
+                                <option value="0">Selecione uma UF</option>
+                            </select>
+                        </div>
+                        <div className="field">
+                            <label htmlFor="">Cidade</label>
+                            <select name="city" id="city">
+                                <option value="0">Selecione uma cidade</option>
+                            </select>
+                        </div>
+                    </div>
+                </fieldset>
+
+                <fieldset>
+                    <legend>
+                        <h2>Descrição</h2>
+                        <span>Fale um pouco sobre você :)</span>
+                    </legend>
+                    
+                </fieldset>
+            </form>
         </div>
     )
 }
