4d6368a39febd05842fcb0fab76bb88016dc5972
fsluizvictor <fs.luizvictor@gmail.com>
2021-02-12 00:07:19 -0300

    finhish first step

--- web/src/pages/CreateGroup/index.tsx
+++ web/src/pages/CreateGroup/index.tsx
@@ -1,10 +1,6 @@
 import React, { useEffect, useState, ChangeEvent, FormEvent } from 'react'
 import { Link, useHistory } from 'react-router-dom'
 import { FiArrowLeft } from 'react-icons/fi'
-import { TileLayer, Marker, Map } from 'react-leaflet'
-import { LeafletMouseEvent } from 'leaflet'
-import { IBGEUFResponse, IBGECITYResponse } from '../../utils/types/types'
-import apiIbge from '../../services/apiIbge'
 import api from '../../services/api'
 
 import Dropzone from '../../components/Dropzone'
@@ -15,74 +11,18 @@ import './styles.css'
 
 const CreateGroup = () => {
 
-    const [ufs, setUfs] = useState<string[]>([])
-    const [cities, setCities] = useState<string[]>([])
-
-    const [selectedUf, setSelectedUF] = useState('0')
-    const [selectedCity, setSelectedCity] = useState('0')
-    const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0])
-
     const [selectedFile, setSelectedFile] = useState<File>()
 
     const [formData, setFormData] = useState({
         name: '',
-        email: '',
-        whatsapp: '',
-        registration: '',
-        birth: '',
-        password: '',
-        description: ''
+        description: '',
+        occupation_area: '',
+        rules: '',
     })
-
-    const [InicialPosition, setInicialPosition] = useState<[number, number]>([0, 0])
-
+  
     const history = useHistory()
 
-    useEffect(() => {
-        navigator.geolocation.getCurrentPosition(position => {
-            const { latitude, longitude } = position.coords
 
-            setInicialPosition([latitude, longitude])
-        })
-    }, [])
-
-    useEffect(() => {
-        apiIbge
-            .get<IBGEUFResponse[]>('estados')
-            .then(response => {
-                const ufInitials = response.data.map(uf => uf.sigla)
-                setUfs(ufInitials)
-            })
-    }, [])
-
-    useEffect(() => {
-        apiIbge
-            .get<IBGECITYResponse[]>(`estados/${selectedUf}/municipios`)
-            .then(response => {
-                const cityNames = response.data.map(city => city.nome)
-                setCities(cityNames)
-            })
-    }, [selectedUf])
-
-
-    function handleSelectUF(event: ChangeEvent<HTMLSelectElement>) {
-        const uf = event.target.value
-
-        setSelectedUF(uf)
-    }
-
-    function handleSelectCity(event: ChangeEvent<HTMLSelectElement>) {
-        const city = event.target.value
-
-        setSelectedCity(city)
-    }
-
-    function handleMapOnClick(event: LeafletMouseEvent) {
-        setSelectedPosition([
-            event.latlng.lat,
-            event.latlng.lng
-        ])
-    }
 
     function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
         const { name, value } = event.target
@@ -91,7 +31,14 @@ const CreateGroup = () => {
 
     }
 
-    function handleTextAreaChange(event: ChangeEvent<HTMLTextAreaElement>) {
+    function handleTextAreaChangeRules(event: ChangeEvent<HTMLTextAreaElement>) {
+        const { name, value } = event.target
+
+        setFormData({ ...formData, [name]: value })
+
+    }
+
+    function handleTextAreaChangeDescription(event: ChangeEvent<HTMLTextAreaElement>) {
         const { name, value } = event.target
 
         setFormData({ ...formData, [name]: value })
@@ -103,33 +50,21 @@ const CreateGroup = () => {
 
         const {
             name,
-            email,
-            whatsapp,
-            registration,
-            birth,
-            password,
-            description
+            description,
+            occupation_area,
+            rules,
         } = formData
-
-        const uf = selectedUf
-        const city = selectedCity
-        const [latitude, longitude] = selectedPosition
 
         const data = new FormData()
 
-
+        const now = new Date()
+        const date = now.getDate()
 
         data.append('name', name)
-        data.append('email', email)
-        data.append('whatsapp', whatsapp)
-        data.append('registration', registration)
-        data.append('birth', birth)
-        data.append('password', password)
         data.append('description', description)
-        data.append('uf', uf)
-        data.append('city', city)
-        data.append('latitude', String(latitude))
-        data.append('longitude', String(longitude))
+        data.append('occupation_area', occupation_area)
+        data.append('date', String(date))
+        data.append('rules', rules)
 
         if (selectedFile) {
             data.append('image', selectedFile)
@@ -137,7 +72,7 @@ const CreateGroup = () => {
 
         console.log(data)
 
-        await api.post('users', data)
+        await api.post('groups', data)
 
         history.push('/')
 
@@ -176,19 +111,19 @@ const CreateGroup = () => {
 
                     <div className="field">
                         <label htmlFor="name">Área de Concentração</label>
-                        <select name="uf" id="uf">
+                        <select name="occupation_area" id="occupation_area">
                             <option value="">Selecione uma área</option>
                         </select>
                     </div>
                     <div className="field">
                         <legend>
                             <h2>Regras</h2>
-                            <span>Fale o comportamento esperado no grupo ;</span>
+                            <span>Fale o comportamento esperado no grupo ;)</span>
                         </legend>
                         <textarea
-                            name="description"
-                            id="description"
-                            onChange={handleTextAreaChange}
+                            name="rules"
+                            id="rules"
+                            onChange={handleTextAreaChangeRules}
                         />
                     </div>
                 </fieldset>
@@ -197,12 +132,12 @@ const CreateGroup = () => {
                     <div className="field">
                         <legend>
                             <h2>Descrição</h2>
-                            <span>Mostre os objetivos do grupo</span>
+                            <span>Escreva sobre os objetivos do grupo  -_-</span>
                         </legend>
                         <textarea
                             name="description"
                             id="description"
-                            onChange={handleTextAreaChange}
+                            onChange={handleTextAreaChangeDescription}
                         />
                     </div>
                 </fieldset>
