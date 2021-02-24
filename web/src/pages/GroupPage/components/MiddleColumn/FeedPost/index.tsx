ade459028148921355973a19eb08d9cb1ced0fd7
fsluizvictor <fs.luizvictor@gmail.com>
2021-02-24 00:03:47 -0300

    add coments

--- web/src/pages/GroupPage/components/MiddleColumn/FeedPost/index.tsx
+++ web/src/pages/GroupPage/components/MiddleColumn/FeedPost/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { ChangeEvent, FormEvent, useState } from 'react';
+import api from '../../../../../services/api';
 
 import Panel from '../../Panel';
 
@@ -15,64 +16,103 @@ import {
   SendIcon,
 } from './styles';
 
+
+
 const FeedPost: React.FC = () => {
+  
+  const [formData, setFormData] = useState({
+    contents: ''
+  })
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault()
+
+    const {
+      contents
+    } = formData
+
+    const data = new FormData()
+
+    data.append('contents', contents)
+
+    await api.post('groups_page/coments/1', data)
+
+  }
+
+  function handleTextAreaChange(event: ChangeEvent<HTMLTextAreaElement>) {
+
+    const { name, value } = event.target
+
+    setFormData({
+      ...formData,
+      [name]: value
+    })
+  }
+
   return (
     <Panel>
       <Container>
-        <Row className="heading">
-          <Avatar src="https://i.imgur.com/81RtXfT.jpg" alt="Rocketseat" />
-          <Column>
-            <h3>Rocketseat</h3>
-            <h4>Instituição de ensino</h4>
-            <time>1 sem</time>
-          </Column>
-        </Row>
-        <PostImage
-          src="https://blog.rocketseat.com.br/content/images/2019/05/Painel.png"
-          alt="Rocketseat Blog"
-        />
+        <form onSubmit={handleSubmit} >
 
-        <Row className="likes">
-          <span className="circle blue" />
-          <span className="circle green" />
-          <span className="circle red" />
-          <span className="number">49</span>
-        </Row>
+          <Row className="heading">
+            <Avatar src="https://i.imgur.com/81RtXfT.jpg" alt="Rocketseat" />
+            <Column>
+              <h3>Rocketseat</h3>
+              <h4>Instituição de ensino</h4>
+              <time>1 sem</time>
+            </Column>
+          </Row>
+          <PostImage
+            src="https://blog.rocketseat.com.br/content/images/2019/05/Painel.png"
+            alt="Rocketseat Blog"
+          />
 
-        <Row>
-          <Separator />
-        </Row>
+          <Row className="likes">
+            <span className="circle blue" />
+            <span className="circle green" />
+            <span className="circle red" />
+            <span className="number">49</span>
+          </Row>
 
-        <Row className="actions">
-          <button>
-            <LikeIcon />
-            <span>Gostei</span>
-          </button>
-          <button>
-            <CommentIcon />
-            <span>Comentar</span>
-          </button>
-          <button>
-            <ShareIcon />
-            <span>Compartilhar</span>
-          </button>
-          <button>
-            <SendIcon />
-            <span>Enviar</span>
-          </button>
-        </Row>
+          <Row>
+            <Separator />
+          </Row>
 
-        <Row>
-          <Separator />
-        </Row>
+          <Row className="actions">
+            <button>
+              <LikeIcon />
+              <span>Gostei</span>
+            </button>
+            <button>
+              <CommentIcon />
+              <span>Comentar</span>
+            </button>
+            <button>
+              <ShareIcon />
+              <span>Compartilhar</span>
+            </button>
+            <button>
+              <SendIcon />
+              <span>Enviar</span>
+            </button>
+          </Row>
 
-        <Row>
-          <Avatar src="https://i.imgur.com/81RtXfT.jpg" alt="Rocketseat" />
-          <textarea />
-          <button>
-            <SendIcon />
-          </button>
-        </Row>
+          <Row>
+            <Separator />
+          </Row>
+
+          <Row>
+            <Avatar src="https://i.imgur.com/81RtXfT.jpg" alt="Rocketseat" />
+            <textarea
+              id="contents"
+              name="contents"
+              onChange={handleTextAreaChange}
+            />
+            <button>
+              <SendIcon />
+            </button>
+          </Row>
+        </form>
 
       </Container>
     </Panel>
